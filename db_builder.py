d7c401e722ec7a174bd3f0396e00f6633e06ccbf
Kevin Li <kevinli@dwightshrute.local>
2017-10-18 15:21:14 -0400

    updated

--- db_builder.py
+++ db_builder.py
@@ -3,25 +3,31 @@ import csv       #facilitates CSV I/O
 
 
 f="discobandit.db"
-
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
 
-nametable = ("CREATE TABLE peepz (name TEXT, age INTEGER, id INTEGER)")          #put SQL statement in this string
+nametable = "CREATE TABLE peepz (name TEXT, age INTEGER, id INTEGER)"          #put SQL statement in this string
 c.execute(nametable)    #run SQL statement
-peepz = csv.DictReader(open("peeps.csv"))
-for row in peepz:
-	add_row = 'INSERT INTO peepz VALUES ("' + row["name"] + '",' + row["age"] + "," + row["id"] + ")"
-	c.execute(add_row)
-
 coursestable = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"          #put SQL statement in this string
 c.execute(coursestable)    #run SQL statement
-courses = csv.DictReader(open("courses.csv"))
+
+peepz = csv.DictReader(open("peeps.csv"))
 for row in peepz:
-	add_row = 'INSERT INTO courses VALUES ("' + row["code"] + '",' + row["mark"] + "," + row["id"] + ")"
-	c.execute(add_row)
+	name = row['name']
+	age = row['age']
+	id = row['id']
+	adduser = "INSERT INTO peepz VALUES (%s, '%s', %s);"%(age, name, id)
+	c.execute(adduser)
+
+courses = csv.DictReader(open("courses.csv"))
+for row in courses:
+    code = row['code']
+    mark = row['mark']
+    id = row['id']
+    addcourse = "INSERT INTO courses VALUES ('%s', %s, %s);"%(code, mark, id)
+    c.execute(addcourse)
 
 #==========================================================
 db.commit() #save changes
